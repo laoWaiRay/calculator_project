b34e768641322ea9a84d4c5124079715084658f0
Ray <raymondly96@gmail.com>
2022-06-14 15:37:57 -0700

    Add icons

--- app.js
+++ app.js
@@ -48,33 +48,73 @@ const isActiveOperator = function(operator){
     return activeOperation.find(obj => obj.name === operator).active;
 }
 
+const checkDisplayOutputLength = () => {
+    if (total.toString().length > 16){
+        display.textContent = total.toFixed(14);
+        return true;
+    }
+}
+
+const updateDisplay = (inputSource) => {
+    if(equalsActive){
+        total = 0;
+        storage = '0';
+        display.textContent = storage;
+        activeOperation.forEach(obj => obj.active = false);
+        equalsActive = false;
+    }
+    if(storage === '0'){
+        storage = inputSource;
+        display.textContent = storage;
+    } else if (storage.length > 15){
+        console.log(storage)
+        return
+    } else if (storage.includes('.') && inputSource === '.'){
+        console.log(storage)
+        return
+    } else{
+        storage += inputSource;
+        display.textContent = storage;
+    }
+    console.log(storage)
+}
+
 const operatorButtonAction = function(operator) {
     switch(true){
         case equalsActive:
             total = total;
+            checkDisplayOutputLength();
             display.textContent = total;
             break;
         case activeOperation.every(obj => obj.active === false):
-            total = parseInt(display.textContent);
+            total = parseFloat(display.textContent);
+            checkDisplayOutputLength();
             display.textContent = total;
             break;
         case isActiveOperator('add'):
-            total = operate(add, total, parseInt(storage));
+            total = operate(add, total, parseFloat(storage));
+            checkDisplayOutputLength();
             display.textContent = total;
             break;
         case isActiveOperator('subtract'):
-            total = operate(subtract, total, parseInt(storage));
+            total = operate(subtract, total, parseFloat(storage));
+            checkDisplayOutputLength();
             display.textContent = total;
             break;
         case isActiveOperator('multiply'):
-            total = operate(multiply, total, parseInt(storage));
+            total = operate(multiply, total, parseFloat(storage));
+            checkDisplayOutputLength();
             display.textContent = total;
             break;
         case isActiveOperator('divide'):
             if(equalsActive === false){
-                total = parseInt(display.textContent);
+                total = parseFloat(display.textContent);
+            } else {
+                total = operate(divide, total, parseFloat(storage));
+                checkDisplayOutputLength();
+                display.textContent = total;
+                break;
             }
-            break;
     }
     storage = '0';
     setActiveOperation(operator);
@@ -83,21 +123,7 @@ const operatorButtonAction = function(operator) {
 
 numInputs.forEach(num => {
     num.addEventListener('click', (e) => {
-        if(equalsActive){
-            total = 0;
-            storage = '0';
-            display.textContent = storage;
-            activeOperation.forEach(obj => obj.active = false);
-            equalsActive = false;
-        }
-        if(storage === '0'){
-            storage = e.target.textContent;
-            display.textContent = storage;
-        } else{
-            storage += e.target.textContent;
-            display.textContent = storage;
-        }
-        console.log(storage)
+        updateDisplay(e.target.textContent)
     })
 })
 
@@ -129,24 +155,88 @@ btnDivide.addEventListener('click', ()=>{
 
 btnEquals.addEventListener('click', ()=>{
    switch(true){
+    case activeOperation.every(obj => obj.active === false):
+            total = parseFloat(display.textContent);
+            checkDisplayOutputLength();
+            if(checkDisplayOutputLength() === true) break;
+            display.textContent = total;
+            break;
     case activeOperation.find(obj => obj.name === 'add').active:
-        total = operate(add, total, parseInt(storage));
+        total = operate(add, total, parseFloat(storage));
+        checkDisplayOutputLength();
+        if(checkDisplayOutputLength() === true) break;
         display.textContent = total;
         break;
     case activeOperation.find(obj => obj.name === 'subtract').active:
-        total = operate(subtract, total, parseInt(storage));
+        total = operate(subtract, total, parseFloat(storage));
+        checkDisplayOutputLength();
+        if(checkDisplayOutputLength() === true) break;
         display.textContent = total;
         break;
     case activeOperation.find(obj => obj.name === 'multiply').active:
-        total = operate(multiply, total, parseInt(storage));
+        total = operate(multiply, total, parseFloat(storage));
+        checkDisplayOutputLength();
+        if(checkDisplayOutputLength() === true) break;
         display.textContent = total;
         break;
     case activeOperation.find(obj => obj.name === 'divide').active:
-        total = operate(divide, total, parseInt(storage));
+        if(storage === '0'){
+            display.textContent = 'Cannot divide by 0';
+            break;
+        }
+        total = operate(divide, total, parseFloat(storage));
+        checkDisplayOutputLength();
+        if(checkDisplayOutputLength() === true) break;
         display.textContent = total;
         break;
    }
    equalsActive = true;
+})
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+    switch(true){
+        case e.code === 'Digit1':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit2':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit3':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit4':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit5':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit6':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit7':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit8':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit9':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Digit0':
+            updateDisplay(e.key);
+            break;
+        case e.code === 'Backspace':
+            storage = storage.slice(0,-1);
+            if (display.textContent.length === 1) {
+                if (display.textContent != '0') {
+                    storage = '0';
+                    display.textContent = storage;
+                }
+                return
+            }
+            display.textContent = storage;
+    }
 })
 
 
